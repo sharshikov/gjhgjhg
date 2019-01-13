69a76ed06092bae57531229429b5e67cc2252cde
user <dsdd>
2019-01-14 02:24:14 +0300

    sadsdasdвыв

--- src/main/java/steps/ScenarioSteps.java
+++ src/main/java/steps/ScenarioSteps.java
@@ -1,6 +1,6 @@
 package steps;
 
-import cucumber.api.java.ru.Когда;
+import cucumber.api.java.en.When;
 
 import java.util.List;
 
@@ -13,52 +13,52 @@ public class ScenarioSteps {
     FilterSteps filterSteps = new FilterSteps();
     ReportSteps reportSteps = new ReportSteps();
 
-    @Когда("^выбран раздел \"(.+)\"$")
+    @When("^выбран раздел \"(.+)\"$")
     public void selectSect(String nameElement) {
         searchSteps.selectSect(nameElement);
     }
 
-    @Когда("^выбрана категория \"(.+)\"$")
+    @When("^выбрана категория \"(.+)\"$")
     public void selectCategory(String nameElement) {
         searchSteps.selectCategory(nameElement);
     }
 
-    @Когда("^выбран раздел электроники: \"(.+)\"$")
+    @When("^выбран раздел электроники: \"(.+)\"$")
     public void selectEletronic(String nameElement) {
         searchSteps.selectEletronic(nameElement);
     }
 
-    @Когда("^заголовок страницы - \"(.+)\"$")
+    @When("^заголовок страницы - \"(.+)\"$")
     public void checkTitleDMSPage(String title) {
         searchSteps.checkPageTitle(title);
     }
 
-    @Когда("^выполнено нажатие на кнопку фильтры$")
+    @When("^выполнено нажатие на кнопку фильтры$")
     public void selectFilter() {
         filterSteps.selectFilter();
     }
 
-    @Когда("^введено значение цены до: \"(.+)\"$")
+    @When("^введено значение цены до: \"(.+)\"$")
     public void setInputPriceTo(String price) {
         filterSteps.setInputPriceTo(price);
     }
 
-    @Когда("^выбраны производители \"(.+)\"$")
+    @When("^выбраны производители \"(.+)\"$")
     public void selectManufacturer(List<String> price) {
         filterSteps.selectManufacturer(price);
     }
 
-    @Когда("^выполнено нажатие на кнопку показать подходящие$")
+    @When("^выполнено нажатие на кнопку показать подходящие$")
     public void showList() {
         filterSteps.showList();
     }
 
-    @Когда("^выполнено нажатие на кнопку показать по 12, если существует кнопка и проверяем кол-во элментов$")
+    @When("^выполнено нажатие на кнопку показать по 12, если существует кнопка и проверяем кол-во элментов$")
     public void searchButton12() {
         reportSteps.searchButton12();
     }
 
-    @Когда("^проверяем есть ли элемент в результате поиска$")
+    @When("^проверяем есть ли элемент в результате поиска$")
     public void check() {
         reportSteps.check();
     }
