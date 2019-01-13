2fa719d2698fdb22d33e5689df13b5ace151896a
user <dsdd>
2019-01-14 02:04:09 +0300

    sadsdasdвыв

--- src/main/java/steps/ScenarioSteps.java
+++ src/main/java/steps/ScenarioSteps.java
@@ -14,39 +14,52 @@ public class ScenarioSteps {
     ReportSteps reportSteps = new ReportSteps();
 
     @Когда("^выбран раздел \"(.+)\"$")
-    public void selectSect(String nameElement){ searchSteps.selectSect(nameElement);
+    public void selectSect(String nameElement) {
+        searchSteps.selectSect(nameElement);
     }
+
     @Когда("^выбрана категория \"(.+)\"$")
-    public void selectCategory(String nameElement){
+    public void selectCategory(String nameElement) {
         searchSteps.selectCategory(nameElement);
     }
 
     @Когда("^выбран раздел электроники: \"(.+)\"$")
-    public void selectEletronic(String nameElement){ searchSteps.selectEletronic(nameElement); }
+    public void selectEletronic(String nameElement) {
+        searchSteps.selectEletronic(nameElement);
+    }
 
     @Когда("^заголовок страницы - \"(.+)\"$")
-    public void checkTitleDMSPage(String title){
+    public void checkTitleDMSPage(String title) {
         searchSteps.checkPageTitle(title);
     }
 
     @Когда("^выполнено нажатие на кнопку фильтры$")
-    public void selectFilter(){ filterSteps.selectFilter(); }
+    public void selectFilter() {
+        filterSteps.selectFilter();
+    }
 
     @Когда("^введено значение цены до: \"(.+)\"$")
-    public void setInputPriceTo(String price){ filterSteps.setInputPriceTo(price); }
+    public void setInputPriceTo(String price) {
+        filterSteps.setInputPriceTo(price);
+    }
 
     @Когда("^выбраны производители \"(.+)\"$")
-    public void selectManufacturer(List<String> price){
+    public void selectManufacturer(List<String> price) {
         filterSteps.selectManufacturer(price);
     }
 
     @Когда("^выполнено нажатие на кнопку показать подходящие$")
-    public void showList(){ filterSteps.showList(); }
+    public void showList() {
+        filterSteps.showList();
+    }
 
     @Когда("^выполнено нажатие на кнопку показать по 12, если существует кнопка и проверяем кол-во элментов$")
-    public void searchButton12(){reportSteps.searchButton12();}
+    public void searchButton12() {
+        reportSteps.searchButton12();
+    }
 
     @Когда("^проверяем есть ли элемент в результате поиска$")
-    public void check (){ reportSteps.check();
+    public void check() {
+        reportSteps.check();
     }
 }
